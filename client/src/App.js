44f427428ac7e9c80b5ab18c6e6eafa4d1930f0a
ivanov9826 <ivanov9826@gmail.com>
2022-08-04 09:16:25 +0300

    edit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+
+
+import { Footer } from './components/common/Footer';
+import { Header } from './components/common/Header'
+import { Search } from './components/search/Search';
+import { UserList } from './components/user-list/UserList';
+
+
+
+function App() {
+    
+
+
+
+    return (
+        <div>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+
+                    <Search />
+                    <UserList />
+                </section>
+
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/services/userService.js
@@ -0,0 +1,30 @@
+
+const baseUrl = "http://localhost:3005/api"
+
+export const getAll = async() => {
+    const response = await fetch(`${baseUrl}/users`);
+    const result = await response.json();
+    
+    return result.users;
+}
+
+export const getOne = async (userId) => {
+    const response = await fetch(`${baseUrl}/users/${userId}`);
+    const result = await response.json();
+
+    return result.user;
+}
+
+export const create = async (userData) => {
+    const response = await fetch(`${baseUrl}/users` , {
+        method:'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+
+    const result = await response.json();
+    console.log(result);
+    return result.user;
+}
